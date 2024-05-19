bdb1164817d8325311b55286ae831091259d80cd
Ahmed Raza <03334121a@gmail.com>
2024-05-19 16:24:01 +0500

    s

--- users_add.php
+++ users_add.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['user'])) header('Location: login.php');
+if (!isset($_SESSION['user'])) header('Location: login.php');
 $user = ($_SESSION['user']);
 ?>
 <?php include 'custom/config.php'; ?>
@@ -13,31 +13,24 @@ $user = ($_SESSION['user']);
   <title>Document</title>
   <script src="https://kit.fontawesome.com/8cd3699074.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="styles/dashboardstyles.css?v=<?= $version ?>" />
+  <link rel="stylesheet" type="text/css" href="styles/user_add.css?v=<?= $version ?>" />
 </head>
 
 <body>
   <div class="Dashboard-page">
     <?php include("partials/app-Dsidebar.php") ?>
     <div class="content">
-    <?php include("partials/app-Dtopbar.php") ?>
+      <?php include("partials/app-Dtopbar.php") ?>
       <div class="content-items">
-        <form action="">
-          <div>
-            <label for="first_name">First name</label>
-            <input type="text" id="first_name" name="first_name">
-          </div>
-          <div>
-            <label for="last_name">Last name</label>
-            <input type="text" id="last_name" name="last_name">
-          </div>
-          <div>
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email">
-          </div>
-          <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-          </div>
+        <form action="" class="user_add_form">
+          <label for="first_name">First name</label>
+          <input type="text" id="first_name" name="first_name">
+          <label for="last_name">Last name</label>
+          <input type="text" id="last_name" name="last_name">
+          <label for="email">Email</label>
+          <input type="text" id="email" name="email">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password">
           <button type="submit"><i class="fa fa-plus"></i>Add User</button>
         </form>
       </div>
